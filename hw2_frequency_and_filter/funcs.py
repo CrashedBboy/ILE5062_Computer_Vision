549e88bfd88b048b17a2c6495dcbdf12bf420daf
CrashedBboy <crashedbboy@gmail.com>
2019-04-04 22:32:52 +0800

    helper function to show two images for comparison

--- hw2_frequency_and_filter/funcs.py
+++ hw2_frequency_and_filter/funcs.py
@@ -6,3 +6,14 @@ import matplotlib.image as mpimg
 def img_show(image):
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.show()
+
+def imgs_compare(image1, image2):
+    figure = plt.figure()
+
+    figure.add_subplot(1,2, 1)
+    plt.imshow(cv2.cvtColor(image1, cv2.COLOR_BGR2RGB))
+
+    figure.add_subplot(1,2, 2)
+    plt.imshow(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))
+    
+    plt.show(block=True)
