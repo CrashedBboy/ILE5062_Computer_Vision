457ce71429d85bba1a944260102a073d5e397cd1
CrashedBboy <crashedbboy@gmail.com>
2019-04-09 22:56:36 +0800

    make filtering codes into functions

--- hw2_frequency_and_filter/funcs.py
+++ hw2_frequency_and_filter/funcs.py
@@ -1,6 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
+import math
 
 # draw & show image
 def img_show(image):
@@ -17,3 +19,67 @@ def imgs_compare(image1, image2):
     plt.imshow(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))
     
     plt.show(block=True)
+
+# shape: tuple, cut_off_ratio: float
+def get_ideal_lp_filter(shape, cut_off_ratio):
+    # declare low pass filter on frequency domain (element values: [0, 1], pass or not)
+    low_pass_filter = np.zeros(shape, dtype=np.bool)
+
+    # compute "how low" the frequency can pass
+    cut_off_frequency = math.ceil(cut_off_ratio * 0.5 * shape[0])
+
+    if (shape[0] > shape[1]):
+        cut_off_frequency = math.ceil(cut_off_ratio * 0.5 * shape[1])
+
+    # write values to filter
+    # we need to shift the index, [0, 1, 2, 3, 4] --> [-2, -1, 0, 1, 2]; [0, 1, 2, 3] -> [-1.5, -0.5, 0.5, 1.5]
+    u_offset = (low_pass_filter.shape[0]-1) / 2
+    v_offset = (low_pass_filter.shape[1]-1) / 2
+
+    for u in range(low_pass_filter.shape[0]):
+        shifted_u = u - u_offset
+
+        for v in range(low_pass_filter.shape[1]):
+            shifted_v = v - v_offset
+            radius_from_center = (shifted_u**2 + shifted_v**2)**(0.5)
+
+            if (radius_from_center <= cut_off_frequency):
+                low_pass_filter[u, v] = 1
+
+    return low_pass_filter
+
+# image: grayscale image or image with only one channel(shape=M*N*1), filter: matrix size must equal to image's 
+def frequency_filtering(image, image_filter):
+
+    # Compute the 2-dimensional discrete Fourier Transform
+    frequency = np.fft.fft2(image)
+
+    # Shift the zero-frequency component to the center of the spectrum (swap half-space)
+    shifted_frequency = np.fft.fftshift(frequency)
+
+    magnitude_spectrum = np.log(np.abs(shifted_frequency))
+
+    filtered_frequency = shifted_frequency * image_filter
+
+    filtered_spectrum = np.log(np.abs(filtered_frequency) + 1) # add 1 to avoid "divided by zero" exception
+
+    filtered_image = np.fft.ifft2(np.fft.ifftshift(filtered_frequency)).real
+
+    # normalize
+    filtered_image = filtered_image - filtered_image.min()
+    filtered_image = (filtered_image / filtered_image.max()) * 255
+    filtered_image = filtered_image.astype(np.uint8)
+
+    return filtered_image, filtered_spectrum
+
+# get frequency domain(shifted) of an image
+def get_image_frequency(image):
+    # Compute the 2-dimensional discrete Fourier Transform
+    frequency = np.fft.fft2(image)
+
+    # Shift the zero-frequency component to the center of the spectrum (swap half-space)
+    shifted_frequency = np.fft.fftshift(frequency)
+
+    magnitude_spectrum = np.log(np.abs(shifted_frequency))
+
+    return magnitude_spectrum
--- hw2_frequency_and_filter/ideal_low_pass_filtering.py
+++ hw2_frequency_and_filter/ideal_low_pass_filtering.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os.path as path
 import math
+import funcs
 
 IMG = './data/task1and2_hybrid_pyramid/cat.jpg'
 
@@ -20,52 +21,12 @@ image = cv2.imread(absolute_img_path, cv2.IMREAD_GRAYSCALE)
 if image.data == None:
     print("invalid image:", absolute_img_path)
     exit()
+ 
+magnitude_spectrum = funcs.get_image_frequency(image) # not necessory, just for showing
 
-# Compute the 2-dimensional discrete Fourier Transform
-frequency = np.fft.fft2(image)
+low_pass_filter = funcs.get_ideal_lp_filter(image.shape, CUT_OFF_RATIO)
 
-# Shift the zero-frequency component to the center of the spectrum (swap half-space)
-shifted_frequency = np.fft.fftshift(frequency)
-
-magnitude_spectrum = np.log(np.abs(shifted_frequency))
-
-# declare low pass filter on frequency domain (element values: [0, 1], pass or not)
-low_pass_filter = np.zeros(frequency.shape, dtype=np.bool)
-
-# compute "how low" the frequency can pass
-cut_off_frequency = math.ceil(CUT_OFF_RATIO * 0.5 * frequency.shape[0])
-
-if (frequency.shape[0] > frequency.shape[1]):
-    cut_off_frequency = math.ceil(CUT_OFF_RATIO * 0.5 * frequency.shape[1])
-
-# write values to filter
-# we need to shift the index, [0, 1, 2, 3, 4] --> [-2, -1, 0, 1, 2]; [0, 1, 2, 3] -> [-1.5, -0.5, 0.5, 1.5]
-u_offset = (low_pass_filter.shape[0]-1) / 2
-v_offset = (low_pass_filter.shape[1]-1) / 2
-
-for u in range(low_pass_filter.shape[0]):
-    shifted_u = u - u_offset
-
-    for v in range(low_pass_filter.shape[1]):
-        shifted_v = v - v_offset
-        radius_from_center = (shifted_u**2 + shifted_v**2)**(0.5)
-
-        if (radius_from_center <= cut_off_frequency):
-            low_pass_filter[u, v] = 1
-
-filtered_frequency = shifted_frequency * low_pass_filter
-
-filtered_spectrum = np.log(np.abs(filtered_frequency) + 1) # add 1 to avoid "divided by zero" exception
-
-filtered_image = np.fft.ifft2(np.fft.ifftshift(filtered_frequency)).real
-
-# normalize
-filtered_image = filtered_image - filtered_image.min()
-filtered_image = (filtered_image / filtered_image.max()) * 255
-filtered_image = filtered_image.astype(np.uint8)
-
-# print(filtered_image.dtype)
-# print(image.dtype), exit()
+filtered_image, filtered_spectrum = funcs.frequency_filtering(image, low_pass_filter)
 
 plt.subplot(2, 2, 1),
 plt.imshow(image, cmap = 'gray')
@@ -80,4 +41,3 @@ plt.subplot(2, 2, 4)
 plt.imshow(filtered_spectrum, cmap = 'gray')
 plt.title('Filtered Spectrum'), plt.xticks([]), plt.yticks([])
 plt.show()
-
