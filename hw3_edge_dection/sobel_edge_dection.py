030ec23dcda4772a9dc6fff4f2dd2b9786f35789
CrashedBboy <crashedbboy@gmail.com>
2019-05-26 17:06:10 +0800

    sobel: calculate edge orientation

--- hw3_edge_dection/sobel_edge_dection.py
+++ hw3_edge_dection/sobel_edge_dection.py
@@ -7,6 +7,8 @@ import funcs
 
 IMAGE = 'data/freedom_gundam_head.jpg'
 
+THRESHOLD = 0.2
+
 image_abs_path = path.abspath(path.join( path.dirname(__file__), IMAGE))
 
 if not path.exists(image_abs_path):
@@ -44,10 +46,32 @@ gradient_y = signal.convolve2d(grayscale_image, sobel_y, boundary='symm', mode='
 
 gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
 
-plt.subplot(1, 2, 1)
+gradient_orientation = np.arctan2(gradient_y, gradient_x)
+
+threshold_mask = np.copy(gradient_magnitude)
+
+threshold = threshold_mask.min() + (threshold_mask.max() - threshold_mask.min()) * THRESHOLD
+
+threshold_mask[threshold_mask >= threshold] = 1
+threshold_mask[threshold_mask < threshold] = 0
+
+threshold_mask.astype(np.bool)
+
+thresholded_magnitude = gradient_magnitude * threshold_mask
+thresholded_orientation = np.arctan2(gradient_y * threshold_mask, gradient_x * threshold_mask)
+
+plt.subplot(1, 3, 1)
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
 plt.imshow(gradient_magnitude, cmap='gray')
 plt.title('Gradient Magnitude'), plt.xticks([]), plt.yticks([])
+plt.subplot(1, 3, 3)
+plt.imshow(thresholded_magnitude, cmap='gray')
+plt.title('Gradient Magnitude(thresholded)'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+plt.imshow(thresholded_orientation, cmap='jet')
+plt.title('edge orientation'), plt.xticks([]), plt.yticks([])
+plt.colorbar()
 plt.show()
