23505c14b312c753f757cf52e86bab7b4b1db9d9
CrashedBboy <crashedbboy@gmail.com>
2019-05-28 16:04:33 +0800

    canny edge dection: using sobel operator

--- hw3_edge_dection/canny_edge_dection.py
+++ hw3_edge_dection/canny_edge_dection.py
@@ -34,8 +34,31 @@ gaussian_kernel = np.exp((-1) * (x**2 + y**2)/(2*(G_SIGMA**2))) / (2*np.pi*(G_SI
 # normalize
 gaussian_kernel = gaussian_kernel / gaussian_kernel.sum()
 
-print("Kernel size:", G_KERNEL_SIZE, ", sigma value:", G_SIGMA, ", Gaussian kernel: \n", gaussian_kernel)
+# print("Kernel size:", G_KERNEL_SIZE, ", sigma value:", G_SIGMA, ", Gaussian kernel: \n", gaussian_kernel)
 
 blurred_image = signal.convolve2d(gray_image, gaussian_kernel, boundary='symm', mode='same')
 
 # step 2. use sobel operator to find edge (gradient)
+
+sobel_x = np.array(
+    [-1, 0, 1,
+    -2, 0, 2,
+    -1, 0, 1]
+    )
+
+sobel_x = sobel_x.reshape((3,3))
+
+sobel_y = np.array(
+    [-1, -2, -1,
+    0, 0, 0,
+    1, 2, 1]
+    )
+
+sobel_y = sobel_y.reshape((3,3))
+
+gradient_x = signal.convolve2d(blurred_image, sobel_x, boundary='symm', mode='same')
+gradient_y = signal.convolve2d(blurred_image, sobel_y, boundary='symm', mode='same')
+
+gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+
+gradient_orientation = np.arctan2(gradient_y, gradient_x)
