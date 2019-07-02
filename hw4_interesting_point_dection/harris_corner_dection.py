d99c5b50de49f533633f14182987b3677d6cf79a
CrashedBboy <crashedbboy@gmail.com>
2019-07-02 16:52:45 +0800

    harris corner dector

--- hw4_interesting_point_dection/harris_corner_dection.py
+++ hw4_interesting_point_dection/harris_corner_dection.py
@@ -5,8 +5,7 @@ from matplotlib import pyplot as plt
 import os.path as path
 import math
 
-
-IMAGE = 'data/triangles.jpg'
+IMAGE = 'data/chessboard.jpg'
 
 G_KERNEL_SIZE = 3
 G_SIGMA = 3
@@ -35,6 +34,42 @@ blurred_image = signal.convolve2d(gray_image, gaussian_kernel, boundary='symm', 
 
 # compute x, y gradiant (using sobel)
 
+sobel_x = np.array([
+    -1, 0, 1,
+    -2, 0, 2,
+    -1, 0, 1
+])
+
+sobel_x = sobel_x.reshape((3,3))
+
+sobel_y = np.array([
+    -1, -2, -1,
+    0, 0, 0,
+    1, 2, 1
+])
+
+sobel_y = sobel_y.reshape((3,3))
+
+gradient_x = signal.convolve2d(blurred_image, sobel_x, boundary='symm', mode='same')
+gradient_y = signal.convolve2d(blurred_image, sobel_y, boundary='symm', mode='same')
+
+gradient_xy = gradient_x * gradient_y
+
 # using gradiant to do harris dection
 
-# thresholding
+derterminant = gradient_x**2 * gradient_y**2 - gradient_xy**2
+trace = gradient_x**2 + gradient_y**2 + (1e-12) # add a small number to avoid "divide by 0"
+
+corner_response = np.abs(derterminant / trace)
+
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(1, 2, 2)
+plt.imshow(corner_response, cmap='gray')
+plt.title('Corner Response'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+# thresholding? non-maximize suppression
